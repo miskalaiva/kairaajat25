be3e634f8638e3c57a3841eb7b8731060ea277d4
Miska Laivamaa <miska@Miskas-MacBook-Pro.local>
2025-08-16 23:27:10 +0300

    netlify to static

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"],
+  modules: ["@nuxtjs/tailwindcss", "@netlify/nuxt"],
   css: ["@/assets/css/main.css"],
   ssr: false, // jos puhdas SPA, tai
   nitro: {
